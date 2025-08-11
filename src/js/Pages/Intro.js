e660c21efe5ce4507e3827ff11e2e1c687d8cdba
Redvus <a.suvorof@gmail.com>
2025-08-11 17:03:01 +0400

    Наметил баннер и писателей.

--- src/js/Pages/Intro.js
+++ src/js/Pages/Intro.js
@@ -19,7 +19,34 @@ export class Intro {
 	initLayoutDev() {
 		this.wrapper.innerHTML = `
 			<div class="wrapper__inside">
-				<div class="wrapper__inside__content">
+				<div class="wrapper__inside_content">
+					<div class="wrapper__inside_top">
+						<div class="wrapper__inside_banner">
+							<img src="/assets/dev/images/in-slider-il2.jpg" alt="Banner Image">
+						</div>
+						<div class="wrapper__inside_anniversary">
+							<div class="wrapper__inside_anniversary_top">
+								<picture class="wrapper__inside_anniversary_image">
+									<img src="/assets/dev/images/anniversary.png" alt="Anniversary Image">
+								</picture>
+								<div class="wrapper__inside_anniversary_card">
+									<div class="wrapper__inside_anniversary_date">
+										<p>10.08.2025</p>
+										<!--<p>130 лет</p>-->
+									</div>
+									<div class="wrapper__inside_anniversary_name">
+										<h4>Михаил<br>Михайлович<br>Зощенко</h4>
+										<p>русский советский писатель, драматург, сценарист, переводчик</p>
+									</div>
+								</div>
+							</div>
+							<div class="wrapper__inside_anniversary_bottom">
+								<p>Нет ничего прекрасней на свете, чем работа, которую делаешь по призванию. Человек, любящий своё дело, достигает больших успехов.</p>
+								<span>«Спустя три года»</span>
+							</div>
+						</div>
+					</div>
+
 					<h1>Title 1</h1>
 					<h2>Title 2</h2>
 					<h3>Title 3</h3>
