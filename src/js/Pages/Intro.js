8f23e7bcb049f6ddf68db7be7b9ac1ac8fc3688a
Redvus <a.suvorof@gmail.com>
2025-08-13 15:37:15 +0400

    Наметил подвал, добавил книги и ресурсы внизу.

--- src/js/Pages/Intro.js
+++ src/js/Pages/Intro.js
@@ -53,15 +53,11 @@ export class Intro {
 				<h2 class="wrapper__content_title">Анонсы</h2>
 				<div class="wrapper__content_news" id="frontAnounces"></div>
 
-				<h1>Title 1</h1>
-				<h2>Title 2</h2>
-				<h3>Title 3</h3>
-				<h4>Title 4</h4>
-				<h5>Title 5</h5>
-				<h6>Title 6</h6>
-				<p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quidem sequi, suscipit, corrupti, accusantium excepturi ab vitae eveniet rerum similique harum! Ratione at voluptates modi aut quo voluptatum suscipit? In iste esse ipsam harum tenetur sunt eius nesciunt numquam hic sit temporibus quos quis, eveniet perferendis fugit ut at laboriosam.</p>
-				<p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quidem sequi, suscipit, corrupti, accusantium excepturi ab vitae eveniet rerum similique harum! Ratione at voluptates modi aut quo voluptatum suscipit? In iste esse ipsam harum tenetur sunt eius nesciunt numquam hic sit temporibus quos quis, eveniet perferendis fugit ut at laboriosam.</p>
+				<h2 class="wrapper__content_title">Что почитать</h2>
+				<div class="wrapper__content_read" id="frontRead"></div>
 
+				<h2 class="wrapper__content_title">Официальные ресурсы</h2>
+				<div class="wrapper__content_bottom" id="frontResources"></div>
 			</div>
 		`;
 
@@ -71,17 +67,17 @@ export class Intro {
 			newsItem.classList.add('wrapper__content_news_item');
 			newsItem.innerHTML = `
 				<picture class="wrapper__content_news_item_top">
-					<img src="/assets/dev/images/news${i + 1}.jpg" alt="News Image ${i + 1}">
+					<img src="/assets/dev/images/newsImageSample.jpeg" alt="News Image ${i + 1}">
 				</picture>
 				<div class="wrapper__content_news_item_bottom">
+					<div class="wrapper__content_news_item_date">
+						<span>10.08.2025</span>
+					</div>
 					<div class="wrapper__content_news_item_title">
-						<h4>Новость ${i + 1}</h4>
+						<h4>Новые правила въезда в Россию</h4>
 					</div>
 					<div class="wrapper__content_news_item_text">
-						<p>Для современного мира сложившаяся структура организации влечёт за собой интересный процесс внедрения модернизации системы массового участия...</p>
-					</div>
-					<div class="wrapper__content_news_item_date">
-						<span>Дата новости ${i + 1}</span>
+						Для современного мира сложившаяся структура организации влечёт за собой интересный процесс внедрения модернизации системы массового участия...
 					</div>
 				</div>
 
@@ -95,22 +91,48 @@ export class Intro {
 			newsItem.classList.add('wrapper__content_news_item');
 			newsItem.innerHTML = `
 				<picture class="wrapper__content_news_item_top">
-					<img src="/assets/dev/images/news${i + 1}.jpg" alt="Anounce Image ${i + 1}">
+					<img src="/assets/dev/images/annImageSample.jpg" alt="Anounce Image ${i + 1}">
 				</picture>
 				<div class="wrapper__content_news_item_bottom">
+					<div class="wrapper__content_news_item_date">
+						<span>10.08.2025</span>
+					</div>
 					<div class="wrapper__content_news_item_title">
-						<h4>Анонс ${i + 1}</h4>
+						<h4>«Авангардная поэзия: мужской и женский взгляд»</h4>
 					</div>
 					<div class="wrapper__content_news_item_text">
 						<p>Для современного мира сложившаяся структура организации влечёт за собой интересный процесс внедрения модернизации системы массового участия...</p>
-					</div>
-					<div class="wrapper__content_news_item_date">
-						<span>Дата анонса ${i + 1}</span>
 					</div>
 				</div>
 
 			`;
 			this.newsItemBlock.appendChild(newsItem);
+		}
+
+		this.readItemBlock = this.wrapper.querySelector('#frontRead');
+		for(let i = 0; i < 5; i++) {
+			const readItem = this.readItemBlock.cloneNode(true);
+			readItem.classList.add('wrapper__content_read_item');
+			readItem.innerHTML = `
+				<picture class="wrapper__content_read_item_image">
+					<img src="/assets/dev/images/bookReadSample.png" alt="Anounce Image ${i + 1}">
+				</picture>
+
+			`;
+			this.readItemBlock.appendChild(readItem);
+		}
+
+		this.resourcesItemBlock = this.wrapper.querySelector('#frontResources');
+		for(let i = 0; i < 6; i++) {
+			const resourcesItem = this.resourcesItemBlock.cloneNode(true);
+			resourcesItem.classList.add('wrapper__content_bottom_item');
+			resourcesItem.innerHTML = `
+				<picture class="wrapper__content_bottom_item_image">
+					<img src="/assets/dev/images/partners/departament1.jpg" alt="Anounce Image ${i + 1}">
+				</picture>
+
+			`;
+			this.resourcesItemBlock.appendChild(resourcesItem);
 		}
 	}
 }
